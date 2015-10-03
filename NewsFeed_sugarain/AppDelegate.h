8c91b635454dfd23cd840bc8a14048e4b85e92f6
SBHR <SBHR@sbhrui-macbook-air.local>
2015-10-03 10:08:21 +0900

    Initial Commit

---
+++ NewsFeed_sugarain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsFeed_sugarain
+//
+//  Created by SBHR on 2015. 10. 3..
+//  Copyright © 2015년 seungbin.baik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsFeed_sugarain/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NewsFeed_sugarain
+//
+//  Created by SBHR on 2015. 10. 3..
+//  Copyright © 2015년 seungbin.baik. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
